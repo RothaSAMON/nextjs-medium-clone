2dba378e7ac11e892834eff353220810da472e16
Rotha SAMON <samonrotha@gmail.com>
2024-11-12 19:12:18 +0700

    Defining users schema adn function

--- components/convex-client-provider.tsx
+++ components/convex-client-provider.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { ReactNode } from "react";
-// import { useAuth } from "@clerk/nextjs";
-import { ConvexReactClient, ConvexProvider } from "convex/react";
-// import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
-    <ConvexProvider client={convex}>
+    <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
       {children}
-    </ConvexProvider>
+    </ConvexProviderWithClerk>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://busy-goldfish-98.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
